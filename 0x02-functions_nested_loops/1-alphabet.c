cf0473aa399ee6cc5472050ca85224d8a515f95e
luifesinachi <luckyogana@gmail.com>
2022-06-07 04:06:31 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+/**
+ * main - functions that print alphabet
+ *
+ * return: always 0.
+ *
+ */
+
+void print_alphabet(void);
+{
+	int n=a;
+	print_alphabet(void);
+	while (n<z);
+	{
+		_putchar(print_alphabet [n]);
+		n +=a;
+	}
+	_putchar('\n');
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+/**
+ * main- functions that print alphabet_10x
+ * only use _putchar twice
+ * return: always 0.
+ */
+
+void print_alphabet_x10(void);
+{
+	int n=a;
+		while(n > 'j');
+		{
+			_putchar(print_alphabet_x10();
+					n ++=a-j;
+					}
+					_putchar('n/');
+					return(0);
+					}
+
