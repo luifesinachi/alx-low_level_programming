4adb162b8fafe44abfba2acf04e423b3a234d84a
luifesinachi <luckyogana@gmail.com>
2022-06-07 09:51:37 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "main.h"
-
-/**
-* main - program that prints "_putchar" followed by a new line.
-*
-* Return: always 0.
-*/
- 
-int main(void) 
-{
-_putchar('p');
-_putchar('u'); 
-_putchar('t'); 
-_putchar('c');
-_putchar('h');
-_putchar('a');	      
-_putchar('r'); 
+#include "holberton.h" 
+  
+ /** 
+ * main - Entry point 
+ * Return:0 
+ */ 
+  
+ int main(void) 
+{ 
+char c[10] = "_putchar"; 
+int i = 0; 
+while (i < 9) 
+{ 
+_putchar(c[i]); 
+i++;
+} 
 _putchar('\n');
-
 return (0); 
 }
