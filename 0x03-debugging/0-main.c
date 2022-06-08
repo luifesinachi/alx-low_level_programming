6a27d79e8fbbdfbfee8f74dbb139cf08b1c820bd
luifesinachi <luckyogana@gmail.com>
2022-06-08 05:21:37 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,32 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
-* main - assign a random number to the variable n each time it is executed
-*
-* if the number is greater than 0: is positive, if the number is 0: is zero
-*
-* if the number is less than 0: is negative, followed by a new line
-*
+* main - tests function that prints if integer is positive or negative
 * Return: 0
-*
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else
-{
-printf("%d is zero\n", n);
-}
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
