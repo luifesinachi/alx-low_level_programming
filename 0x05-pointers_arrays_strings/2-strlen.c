1829e145055aa071f0c8ea2c629d8e32d6110330
luifesinachi <luckyogana@gmail.com>
2022-06-13 08:50:18 -0700

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,9 +12,6 @@ int main (void)
 char str[50];
 int len;
 
-strcpy(str, "This is tutorialspoint.com");
-
-len = strlen(str);
 printf("Length of |%s| is |%d|\n", str, len);
 		          
 return(0);
