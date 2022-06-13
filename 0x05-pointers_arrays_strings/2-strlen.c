8174c1dac3300b21b95a2d5916cc5a81f12aa3ef
luifesinachi <luckyogana@gmail.com>
2022-06-13 08:17:37 -0700

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,21 @@
 #include "main.h"
+#include <studio.h>
 
 /** 
 * _strlen - Returns the length of a string
 * @str: The string to get the length of. 
 * 
-* Return: The length of @str. 
-*/ 
-size_t _strlen(const char *str) 
-{ 
-size_t length = 0; 
-		    
-while (*str++) 
-length++; 
-		     
-return (length); 
+* Return: 0.
+*/
+int main () 
+{
+char str[50];
+int len;
+
+strcpy(str, "This is tutorialspoint.com");
+
+len = strlen(str);
+printf("Length of |%s| is |%d|\n", str, len);
+		          
+return(0);
 }
