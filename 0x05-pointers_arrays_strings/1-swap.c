c132dd73fc6c9f14789c2e3078e40d7034ea334b
luifesinachi <luckyogana@gmail.com>
2022-06-13 05:47:12 -0700

    updated

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
-/**
-* swap_int - check the code for Holberton School students.
-* @a: is a pointer int type
-* @b: is a pointer int type
-* Return: Always 0.
-*/
-
-void swap_int(int *a, int *b)
-{
-int c;
-
-c = *a;
-*a = *b;
-*b = c;
+/** 
+* swap_int - main 
+* @a: pointer
+* @b: pointer 
+*/ 
+  
+void swap_int(int *a, int *b) 
+{ 
+int swap; 
+		    
+swap = *a; 
+*a = *b; 
+*b = swap; 
 }
