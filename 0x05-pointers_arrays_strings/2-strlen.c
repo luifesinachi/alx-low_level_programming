34603a1d9213c54ba639aa805538b174880aaa27
luifesinachi <luckyogana@gmail.com>
2022-06-14 16:09:57 -0700

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-/** 
-* _strlen - Returns the length of a string
-* @str: The string to get the length of. 
-* 
-* Return: 0.
-*/
-int main (void)	
-{
-char str[50];
-int len;
 
-printf("Length of |string| is |6|\n", str, len);
-		          
-return(0);
+/** 
+* _strlen - function Returns the length of a string
+* @s: pointer to an string
+* 
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+i += 1;
+									}
+return (i);
 }
