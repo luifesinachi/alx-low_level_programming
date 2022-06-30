c75ebe7c3e3f3e6767db9bed0f93b2cda6d57d0c
luifesinachi <luckyogana@gmail.com>
2022-06-30 06:44:18 -0700

    my first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * malloc_check - function that allocates memory using malloc
+ * @b: size of int
+ * return: nothing
+ */
+void *malloc_check(unsigned  int b);
+{
+void *al;
+
+al = malloc(b);
+if (al == NULL)
+exit(98);
+else
+return (al);
+}
