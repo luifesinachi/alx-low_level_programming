60c06192ed29de0fe9ab2e95fad3ee4ed041028c
luifesinachi <luckyogana@gmail.com>
2022-06-30 21:18:00 -0700

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,10 +15,10 @@ unsigned int i;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
-p = malloc(nmemb * size);
+p = malloc(nmemb *size);
 if (p == NULL)
 									return (NULL);
-for (i = 0; i < (nmemb * size); i++)
+for (i = 0; i < (nmemb *size); i++)
 p[i] = 0;
 return (p);
 }
