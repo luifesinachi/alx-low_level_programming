cbf84d4691b2583e0799eeb1634f0d29256b9b9c
luifesinachi <luckyogana@gmail.com>
2022-07-14 20:21:39 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,40 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
-* _strlen - Returns the lenght of a string.
-* @s: Type char pointer
-* Return: Always 0.
+* add_node - Adds a new node at the beginning
+*            of a list_t list.
+* @head: A pointer to the head of the list_t list.
+* @str: The string to be added to the list_t list.
+*
+* Return: If the function fails - NULL.
+*         Otherwise - the address of the new element.
 */
 
-int _strlen(const char *s)
-{
-unsigned int len;
-
-for (len = 0; s[len] != '\0'; len++)
-{
-}
-return (len);
-}
-
-/**
-* add_node - Return the number of elements in a linked list
-* @head: data type poointer of struct node
-* @str: data type pointer of struct
-* Return: elements of the str i
-*/
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *n_node;
-
-n_node = malloc(sizeof(list_t));
-
-if (n_node == NULL)
+char *dup;
+int len;
+list_t *new;
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
+dup = strdup(str);
+if (dup == NULL)
+{
+free(new);
+return (NULL);
+}
+for (len = 0; str[len];)
+len++;
 
-n_node->str = strdup(str);
-n_node->len = _strlen(str);
-n_node->next = *head;
-*head = n_node;
-return (n_node);
+new->str = dup;
+new->len = len;
+new->next = *head;
+
+*head = new;
+
+return (new);
 }
