8c7e32de45629116b76bd18dbe022b642734f9a3
luifesinachi <luckyogana@gmail.com>
2022-07-14 19:15:15 -0700

    updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
 * @len: length of the string
 * @next: points to the next node
 * Description: singly linked list node structure
-* for Holberton project
+* for Alx project
 */
+
 typedef struct list_s
 {
 char *str;
