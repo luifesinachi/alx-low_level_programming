10ab471d427e54280d4aa4d00e797c82701cf8e5
luifesinachi <luckyogana@gmail.com>
2022-07-21 15:05:40 -0700

    my second commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-* binary_to_uint - converts a binary number into an unsigned int
-* @b: type const char pointer of one bit
-* Return: converted number, or 0 if one or more char in str b that is not 0/1
+* _pow_recursion - function that returns the value of x
+* raised to the power of y
+* @x: base number
+* @y: pow number
+* Return: int
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+* _strlen - function that returns the length of a string.
+*
+* @s: pointer to an string
+* Return: int
+*/
+
+int _strlen(const char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+i += 1;
+}
+return (i);
+}
+
+/**
+* binary_to_uint - function that converts a binary number to an unsigned int
+* @b: pointing to a string of 0 and 1 chars
+* Return: the converted number, or 0 if
+* there is one or more chars in the string b that is not 0 or 1
+* b is NULL
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int n = 0;
-unsigned int dec = 0;
+int len, exp = 0;
+unsigned int res = 0;
 
 if (b == NULL)
 return (0);
-while (*(b + n) != '\0')
+len = _strlen(b);
+while (len-- && len >= 0)
 {
-if (b == NULL)
+if (b[len] == '1')
+res += _pow_recursion(2, exp);
+else if (b[len] != '0')
 return (0);
-while (*(b + n) != '\0')
-{
-if (*(b + n) != '0' && *(b + n) != '1')
-return (0);
-dec <<= 1;
-if (*(b + n) == '1')
-dec ^= 1;
-n++;
+exp++;
 }
-return (dec);
+return (res);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,3 +16,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
+#endif
