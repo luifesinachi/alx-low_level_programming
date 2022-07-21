ed27e229a7cc07cb64d880024e3ce95e99015f98
luifesinachi <luckyogana@gmail.com>
2022-07-21 16:13:55 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,47 +3,28 @@
 #include "main.h"
 
 /**
-* _pow_recursion - function that returns the value of x
-* raised to the power of y
-* @x: base number
-* @y: pow number
-* Return: int
+* _right_pbin_mask - recursion function to print bit 0 (acts as a mask)
+* @n: type const unsigned long int
 */
 
-int _pow_recursion(int x, int y)
+void _right_pbin_mask(unsigned long int n)
 {
-if (y < 0)
-return (-1);
-if (y == 0)
-return (1);
-return (x * _pow_recursion(x, y - 1));
+if (n == 0)
+return;
+_right_pbin_mask(n >> 1);
+_putchar((n & 1) + '0');
 }
 
 /**
-* print_binary - function that prints the binary representation of a number
-* @n: decimal number
-* Return: nothing
+* print_binary - prints the binary representation of a number
+* @n: type const unsigned long int
+* Return: binary number
 */
 
 void print_binary(unsigned long int n)
 {
-unsigned int res_pow = 0;
-int exp = 10;
-int flag = 0;
-
 if (n == 0)
 _putchar('0');
-while (exp >= 0)
-{
-res_pow = _pow_recursion(2, exp);
-if (n >= res_pow)
-{
-_putchar('1');
-n -= res_pow;
-flag = 1;
-}
-else if (n < res_pow && flag == 1)
-_putchar('0');
-exp--;
-}
+else
+_right_pbin_mask(n);
 }
